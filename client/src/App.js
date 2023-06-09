127c5cb1c8cd6ad4dc4494c5591adc95d9e97c05
daman917 <daman.sohi@ymail.com>
2023-06-08 21:28:57 -0700

    Added Listing component which can expand on click

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import Listing from './Listing';
+
 const App = () => {
   return (
     <div className="App">
-      <h1 className="text-blue-500"> React App</h1>
+      <Listing
+        address="123 Main St"
+        price="$500,000"
+        owner="John Doe"
+        imageSrc="https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg"
+        openPools={5}
+        onDetailsClick={() => {
+          // Handle onDetailsClick event
+        }}
+      />
     </div>
   );
 };
