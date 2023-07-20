ef6f5a3f10377dd9e0969b8a0bc0794e2ea9d6bb
sashankshukla <ayaz.shukla@gmail.com>
2023-07-20 19:02:28 -0400

    slight color change

--- client/src/components/UserDashboard/AddFunds.jsx
+++ client/src/components/UserDashboard/AddFunds.jsx
@@ -113,7 +113,7 @@ const AddFunds = () => {
           <div className="pt-5">
             <button
               type="submit"
-              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md group hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-700 border border-transparent rounded-md group hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
             >
               Confirm Transfer
             </button>
